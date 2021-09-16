fc44b60fc20a5ce916a269d31b56cd53807a6972
Santiago29-6 <chavarrokevin29@gmail.com>
2021-09-16 16:24:51 -0500

    Subir archivo

---
+++ src/parcial_multinel_supermercado/Parcial_Multinel_SuperMercado.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial_multinel_supermercado;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Parcial_Multinel_SuperMercado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
